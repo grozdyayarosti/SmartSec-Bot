c5735241a218ed5ff47e7b08cfcd4499eb37c60c
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2025-03-16 19:07:30 +0500

    Добавил запись результатов тестирования в БД

--- smartsec testing/main.py
+++ smartsec testing/main.py
@@ -1,11 +1,20 @@
 import telebot
 import psycopg2
+from telebot import types
 
 from constants import TELEGRAM_BOT_TOKEN, PG_DBNAME, PG_USER, PG_PASSWORD, PG_HOST, PG_PORT
 from funcs import delete_ReplyKeyboard
 
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+active_quizzes : dict[str:types.Poll] = dict()
+conn = psycopg2.connect(
+    dbname=PG_DBNAME,
+    user=PG_USER,
+    password=PG_PASSWORD,
+    host=PG_HOST,
+    port=PG_PORT
+)
 
 # Обработчик события при получении команды start
 @bot.message_handler(commands=['start'])
@@ -28,13 +37,6 @@ def dialog(message):
         match message.text:
             case '1':
 
-                conn = psycopg2.connect(
-                    dbname=PG_DBNAME,
-                    user=PG_USER,
-                    password=PG_PASSWORD,
-                    host=PG_HOST,
-                    port=PG_PORT
-                )
                 cur = conn.cursor()
                 cur.execute("SELECT * FROM questions;")
                 question = cur.fetchone()
@@ -50,24 +52,26 @@ def dialog(message):
                 )
                 answers = cur.fetchall()
                 answers_text = []
-                correct_index = 0
+                correct_answer = None
                 for index, (answer, is_correct) in enumerate(answers):
                     answers_text.append(answer)
                     if is_correct:
-                        correct_index = index
+                        correct_answer = index
 
                 conn.commit()
                 cur.close()
-                conn.close()
+                # conn.close()
 
-                bot.send_poll(
+                poll_message = bot.send_poll(
                     message.chat.id,
                     question_text,
                     answers_text,
                     type='quiz',
-                    correct_option_id=correct_index,
+                    is_anonymous=False,
+                    correct_option_id=correct_answer,
                     explanation='Это в первую очередь обеспечение доступа к информации только авторизованным лицам'
                 )
+                active_quizzes[message.chat.username] = poll_message.poll
             case _:
                 bot.send_message(
                     message.chat.id,
@@ -75,15 +79,36 @@ def dialog(message):
                 )
 
 
-@bot.callback_query_handler(func=lambda callback: callback.data)
-def check_callback_data(callback):
-    if callback.data == 'right answer':
-        delete_ReplyKeyboard(callback.message, bot)
-        bot.send_message(callback.message.chat.id, 'Правильный ответ!')
-    elif callback.data == 'wrong answer':
-        delete_ReplyKeyboard(callback.message, bot)
-        bot.send_message(callback.message.chat.id, 'Неправильный ответ!')
-
+@bot.poll_answer_handler()
+def handle_poll_answer(quiz_answer: types.PollAnswer):
+    checked_poll = active_quizzes.get(quiz_answer.user.username, dict())
+    if quiz_answer.poll_id == checked_poll.id:
+        is_correct_answer = checked_poll.correct_option_id == quiz_answer.option_ids[0]
+        poll_question = checked_poll.question
+        # TODO добавить дату ответа на вопрос
+        query = f'''
+            INSERT INTO testing_results(user_id, question_id, is_correct_answer)
+                VALUES(
+                    (
+                     SELECT id 
+                       FROM public.users 
+                      WHERE login = '{quiz_answer.user.username}'
+                    ),
+                    (
+                     SELECT id 
+                       FROM public.questions 
+                      WHERE text = '{poll_question}'
+                    ),
+                    {is_correct_answer}
+                )
+        '''
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        bot.send_message(
+            quiz_answer.user.id,
+            "Ответ записан"
+        )
 
 print('START')
 # Заставляет бота работать бесперебойно(пока на машине запущен код)
