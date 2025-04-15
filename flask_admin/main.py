34cf64de24e15b9e1dd7c00537f03081d696e631
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2025-04-15 23:22:48 +0500

    Добавил инструкцию на стартовую страницу админки

--- flask_admin/main.py
+++ flask_admin/main.py
@@ -1,10 +1,27 @@
 from flask import Flask
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
 
-from models import User, Question, Answer, TestingResult, AnswerResult, AnswerResultsView, TestingResultsView, UserView
+from models import User, Question, Answer, TestingResult, AnswerResult
+from model_views import AnswerResultsView, TestingResultsView, UserView
 from smartsec_testing.constants import PG_USER, PG_PASSWORD, PG_HOST, PG_DBNAME, FLASK_SECRET_KEY
+
+
+class StatsView(AdminIndexView):
+    @expose('/statistics')
+    def index(self):
+        users_count = db.session.query(User).count()
+        questions_count = db.session.query(Question).count()
+        return self.render('admin/statistics.html',
+                          users_count=users_count,
+                          questions_count=questions_count)
+
+class ManualView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/manual.html')
+
 
 app = Flask(__name__)
 # Конфигурация БД (замените на свои параметры)
@@ -19,8 +36,12 @@ db = SQLAlchemy(app)
 # metadata = MetaData(bind=engine)
 
 # Настройка админки
-admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
+admin = Admin(app,
+              name='Admin Panel',
+              template_mode='bootstrap3',
+              index_view=ManualView())
 
+# admin.add_view(StatsView(db.session))
 admin.add_view(ModelView(Question, db.session))
 admin.add_view(ModelView(Answer, db.session))
 admin.add_view(AnswerResultsView(AnswerResult, db.session))
