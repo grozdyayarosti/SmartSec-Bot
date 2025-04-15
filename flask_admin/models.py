8fe6632b4887466d0ab2583ffae3b60803f99f55
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2025-04-15 22:23:24 +0500

    Реализовал сортировку для всех полей + подчистил комментарии

--- flask_admin/models.py
+++ flask_admin/models.py
@@ -4,33 +4,41 @@ from sqlalchemy.orm import DeclarativeBase, Relationship
 
 # TODO прописать Nullable-атрибуты
 class AnswerResultsView(ModelView):
-    column_list = ['question', 'answer', 'is_correct']  # Явно перечисляем столбцы
-    form_columns = ['question', 'answer', 'is_correct']             # Показываем в форме
+    column_list = ['question', 'answer', 'is_correct']  # Отображаемые в модели столбцы
+    form_columns = ['question', 'answer', 'is_correct'] # Отображаемые в форме столбцы
     form_ajax_refs = {
         'question': {
-            'fields': ['text', 'explanation'],  # Какие поля показывать в поиске
-            # 'page_size': 10,  # Пагинация
-            'placeholder': 'Выберите вопрос...'
+            'fields': ['text', 'explanation'],   # Отображаемые в поиске столбцы
+            # 'page_size': 10,                   # Пагинация
+            'placeholder': 'Выберите вопрос...'  # Отображаемая в поиске подсказка
         },
         'answer': {
-            'fields': ['text'],  # Какие поля показывать в поиске
-            # 'page_size': 10,  # Пагинация
+            'fields': ['text'],
             'placeholder': 'Выберите ответ...'
         }
     }
+    column_sortable_list = (
+        ('question', 'question.text'), ('answer', 'answer.text'), 'is_correct'
+    )
 
 class TestingResultsView(ModelView):
-    column_list = ['login', 'question', 'is_correct_answer', 'answer_date']  # Явно перечисляем столбцы
-    can_create = False  # Запретить создание
-    can_edit = False    # Запретить редактирование
-    can_delete = False  # Опционально: запретить удаление
+    column_list = ['login', 'question', 'is_correct_answer', 'answer_date']
+    column_sortable_list = (
+        'login', ('question', 'question.text'), 'is_correct_answer', 'answer_date'
+    )
+    can_create = False
+    can_edit = False
+    can_delete = False
 
 
 class UserView(ModelView):
-    can_create = False  # Запретить создание
-    can_edit = False    # Запретить редактирование
-    can_delete = False  # Опционально: запретить удаление
-
+    column_list = ['id', 'login', 'is_completed']
+    column_sortable_list = (
+        'id', 'login', 'is_completed'
+    )
+    can_create = False
+    can_edit = False
+    can_delete = False
 
 class BaseModel(DeclarativeBase): pass
 
