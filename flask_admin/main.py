0ece353eb84e465ad7b2823fdb8ff8bbcef9b301
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2025-04-17 23:49:51 +0500

    Пофиксил отображение вкладки статистики в админке

--- flask_admin/main.py
+++ flask_admin/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_admin import Admin, AdminIndexView, expose
+from flask_admin import Admin, AdminIndexView, expose, BaseView
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
 
@@ -8,14 +8,15 @@ from model_views import AnswerResultsView, TestingResultsView, UserView
 from smartsec_testing.constants import PG_USER, PG_PASSWORD, PG_HOST, PG_DBNAME, FLASK_SECRET_KEY
 
 
-class StatsView(AdminIndexView):
-    @expose('/statistics')
+class StatsView(BaseView):
+    @expose('/')
     def index(self):
         users_count = db.session.query(User).count()
         questions_count = db.session.query(Question).count()
         return self.render('admin/statistics.html',
                           users_count=users_count,
                           questions_count=questions_count)
+
 
 class ManualView(AdminIndexView):
     @expose('/')
@@ -24,7 +25,6 @@ class ManualView(AdminIndexView):
 
 
 app = Flask(__name__)
-# Конфигурация БД (замените на свои параметры)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DBNAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = FLASK_SECRET_KEY
@@ -32,16 +32,13 @@ app.config['SECRET_KEY'] = FLASK_SECRET_KEY
 # app.config['DEBUG'] = True  # Детальные ошибки в браузере
 # TODO вынести db в models.py
 db = SQLAlchemy(app)
-# engine = create_engine(f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DBNAME}')
-# metadata = MetaData(bind=engine)
 
-# Настройка админки
 admin = Admin(app,
               name='Admin Panel',
               template_mode='bootstrap3',
-              index_view=ManualView())
+              index_view=ManualView(name='Home'))
 
-# admin.add_view(StatsView(db.session))
+admin.add_view(StatsView(name='Statistics', endpoint='statistics'))
 admin.add_view(ModelView(Question, db.session))
 admin.add_view(ModelView(Answer, db.session))
 admin.add_view(AnswerResultsView(AnswerResult, db.session))
